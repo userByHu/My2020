2f93da1995d12001aa56e11fde887a7d0a034653
hujianghao_mac <15021074732@163.com>
2019-10-30 00:14:57 +0800

    git上传到哪里去了，15021074732@163.com的账号没找到啊；

--- as_for_learn/AppByHu2019/app/src/main/java/com/appbyhu2019/MainActivity.java
+++ as_for_learn/AppByHu2019/app/src/main/java/com/appbyhu2019/MainActivity.java
@@ -3,12 +3,17 @@ package com.appbyhu2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("TAG","git上传到哪里去了？");
     }
 }
