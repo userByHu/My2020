a8800a39717647090a7d7ee3c42ef1b6f90811bc
hujianghao_mac <15021074732@163.com>
2020-02-13 17:59:00 +0800

    主界面底部效果实现：自定义注解、自定义动画；

---
+++ My2020/app/src/main/java/hujianghao/my2020/BaseActivity.java
@@ -0,0 +1,29 @@
+package hujianghao.my2020;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.ButterKnife;
+
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ViewInject annotation = this.getClass().getAnnotation(ViewInject.class);
+        if(annotation != null){
+            int mainlayoutid = annotation.mainlayoutid();
+            if(mainlayoutid > 0){
+                setContentView(mainlayoutid);
+                ButterKnife.bind(this);
+            }else {
+                throw new RuntimeException("mainlayoutid < 0");
+            }
+        }else {
+            throw new RuntimeException("annotation is n ull");
+        }
+    }
+}
---
+++ My2020/app/src/main/java/hujianghao/my2020/ViewInject.java
@@ -0,0 +1,19 @@
+package hujianghao.my2020;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME) // 运行时注解
+@Target(ElementType.TYPE) // 类 接口 注解
+public @interface ViewInject {
+
+    int mainlayoutid() default -1;
+}
+
+
+
+
